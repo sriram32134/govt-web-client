b976db629628f1509d3c4df32d93dea5090a78c5
kalyanreddy <epandagow48@gmail.com>
2025-12-26 10:20:40 +0530

    checking prototype

--- src/components/Home/District.jsx
+++ src/components/Home/District.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "../../styles/home.css";
+import "../../styles/Home.css";
 
 function District() {
   const districts = [
